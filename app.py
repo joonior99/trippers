1e398d06e1cc074454dec67a80ec78cdb397bab8
Choice <wer1264@gmail.com>
2021-06-08 16:58:53 +0900

    로그인, 회원가입 연결

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")
+client = MongoClient('localhost', 27017)
 db = client.dbtrippers
 
 
 @app.route('/')
 def main():
     return render_template("login.html")
+
+@app.route('/membership')
+def membership():
+    return render_template('membership.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
