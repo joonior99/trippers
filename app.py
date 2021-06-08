028de806bcf279bc67e71be0a0ae2bcd02e1bfa9
joonior99 <hjyny@naver.com>
2021-06-08 21:34:06 +0900

    회원가입

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, jsonify
-from pymongo import MongoClient
-
-
 app = Flask(__name__)
 
+from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbtrippers
 
@@ -15,6 +13,7 @@ def main():
 @app.route('/membership')
 def membership():
     return render_template('membership.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
