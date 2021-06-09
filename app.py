21f4a11777154334ef82cc53eedfc639bb82439a
Choice <wer1264@gmail.com>
2021-06-09 12:01:14 +0900

    메인페이지, 리뷰페이지 페이지 간 이동

--- app.py
+++ app.py
@@ -72,6 +72,14 @@ def sign_in():
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
+@app.route("/write_review")
+def write_review():
+    return render_template("review.html")
+
+@app.route("/go_main")
+def go_main():
+    return render_template("mainpage.html")
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
